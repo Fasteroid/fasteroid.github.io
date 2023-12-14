bbb99acc17837b66a9f0ed864a151a5de8254211
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2023-12-14 17:17:05 -0500

    rename

--- src/routes/expression2/+page.ts
+++ src/routes/expression2/+page.ts
@@ -2,6 +2,5 @@ import { load as loadParent } from "../+layout";
 import { defineData } from "$lib/pagedata"
 
 export const load = defineData( loadParent, {
-	title: "E2 Projects",
-	longTitle: "Expression 2"
+	title: "Expression 2"
 } )
