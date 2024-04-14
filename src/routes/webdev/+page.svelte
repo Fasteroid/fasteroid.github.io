c3df01f2c49ab4454430c5c3b3360ea3167f8365
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2024-04-13 20:25:46 -0400

    NEW SECTION: Web Stuff

---
+++ src/routes/webdev/+page.svelte
@@ -0,0 +1,13 @@
+<script>
+    import { base } from "$app/paths";
+    import ProjectCollection from "../../components/projectcollection/ProjectCollection.svelte";
+</script>
+
+<svelte:head>
+    <style lang="scss">
+        // @import "./e2-project.scss";
+    </style>
+</svelte:head>
+
+<ProjectCollection topic="webdev">
+</ProjectCollection>
