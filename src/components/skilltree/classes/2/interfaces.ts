6d43c34adb2f53392b889058d4bcbe787058d178
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2024-08-10 00:07:05 -0400

    finish abstraction efforts

--- src/components/skilltree/classes/2/interfaces.ts
+++ src/components/skilltree/classes/2/interfaces.ts
@@ -1,16 +1,16 @@
-export interface GraphNodeRelation {
-    to:   (string | number)[]
-    from: (string | number)[]
-}
-
 /**
  * Dataset for a graph of nodes. Associates data and relations with node IDs.
  */
 export interface GraphNodeDataset<T extends GraphNodeData> {
-    data:      { [key: string | number]: T }
-    relations: { [key: string | number]: GraphNodeRelation }
+    nodes: { [key: string | number]: T }
+    edges: GraphEdgeData[]
 }
 
 export interface GraphNodeData {
     id: string | number
+}
+
+export interface GraphEdgeData {
+    node1: string | number
+    node2: string | number
 }
--- src/components/skilltree/classes/2/v2.ts
+++ src/components/skilltree/classes/2/v2.ts
@@ -1,79 +1,98 @@
 import { clamp, Map2D, Set2D, Vec2 } from "../utils";
-import { GraphNodeData, GraphNodeDataset } from "./interfaces";
+import { GraphEdgeData, GraphNodeData, GraphNodeDataset } from "./interfaces";
 
 /**
  * This new class encompasses the most basic functionalities
  * of the old SkillTreeNodeManager class.
  * 
- * @param T - Minimum specs for node data in this graph.
- * @param N - Minimum specs for node methods in this graph.  Leave blank for bare minimum.
+ * @param Data - Minimum specs for node data in this graph.
+ * @param Node - Minimum specs for node methods in this graph.  Leave blank for bare minimum.
+ * @param Edge - Minimum specs for edge methods in this graph.  Leave blank for bare minimum.
  */
-export abstract class GraphManager<Data extends GraphNodeData, Node extends GraphNode<Data> = GraphNode<Data>> {
-
-    public readonly relations: GraphNodeDataset<Data>['relations'];
+export abstract class GraphManager<
+    Data     extends GraphNodeData = GraphNodeData,
+    EdgeData extends GraphEdgeData = GraphEdgeData,
+    Edge     extends GraphEdge<EdgeData> = GraphEdge<EdgeData>,
+    Node     extends GraphNode<Data, Edge> = GraphNode<Data, Edge>
+> {
     public readonly template:  HTMLElement;
-    public readonly container: HTMLElement;
 
-    public readonly nodes:     Map<string | number, Node> = new Map();
+    public readonly nodeContainer: HTMLElement;
+    public readonly edgeContainer: SVGSVGElement;
 
-    /**
-     * This method is called by the constructor to create a new node.
-     * 
-     * @param manager - The GraphManager instance that is creating the node.
-     * @param data    - The data to associate with the new node.
-     * 
-     * @returns A new GraphNode instance.
-     */
-    protected abstract createNode(manager: GraphManager<Data, Node>, data: Data): Node;
+    public readonly nodes: Map<string | number, Node> = new Map();
+    public readonly edges: Edge[] = [];
 
-    protected abstract createEdge(): GraphEdge;
+    protected abstract createNode(data: Data): Node;
+    protected abstract createEdge(data: GraphEdgeData): Edge;
 
     private _frame = () => {
         this.nodes.forEach( node => node.render() );
+        this.edges.forEach( edge => edge.render() );
     }
 
     constructor(
         template: HTMLElement, 
-        container: HTMLElement,
+        nodeContainer: HTMLElement,
+        edgeContainer: SVGSVGElement,
         data: GraphNodeDataset<Data>
     ){
-        this.relations = data.relations;
         this.template  = template;
-        this.container = container;
+        this.nodeContainer = nodeContainer;
+        this.edgeContainer = edgeContainer;
 
-        for(const id in data.data){
-            this.nodes.set(id, this.createNode(this, data.data[id]));
+        for(const id in data.nodes){
+            this.nodes.set(id, this.createNode(data.nodes[id]));
         }
 
-        // TODO: edges
-
-        // // from -> to
-        // let edges: Set2D<string | number> = new Set2D();
-        // for(const id in this.relations){
-        //     let relation = this.relations[id];
-        //     for(const to of relation.to){
-        //         edges.add(id, to);
-        //     }
-        //     for(const from of relation.from){
-        //         edges.add(from, id);
-        //     }
-        // }
+        for(const edgeData of data.edges){
+            let edge = this.createEdge(edgeData);
+            this.nodes.get(edgeData.node1)!.edges.push(edge);
+            this.nodes.get(edgeData.node2)!.edges.push(edge);
+            this.edges.push(edge);
+        }
 
         setInterval( () => {
             this.nodes.forEach( node => node.doForces() )
+            this.edges.forEach( edge => edge.doForces() )
             this.nodes.forEach( node => node.doPositioning() )
+            this.nodes.forEach( node => node.clampToContainer() )
             requestAnimationFrame(this._frame);
         } , 16);
     }
 
 }
 
-export abstract class GraphEdge<Data extends GraphNodeData> {
+export abstract class GraphEdge<EdgeData extends GraphEdgeData = GraphEdgeData> {
 
-    svg: SVGLineElement;
+    protected svg: SVGLineElement;
 
-    constructor(){
+    protected node1: GraphNode;
+    protected node2: GraphNode;
+
+    constructor(
+        manager: GraphManager<GraphNodeData, EdgeData, GraphEdge, GraphNode>,
+        data:    EdgeData
+    ){
         this.svg = document.createElementNS("http://www.w3.org/2000/svg", "line");
+        this.svg.classList.add("graph-edge");
+
+        this.node1 = manager.nodes.get(data.node1)!;
+        this.node2 = manager.nodes.get(data.node2)!;
+
+        manager.edgeContainer.appendChild(this.svg);
+    }
+
+    /**
+     * Called once per simulation frame; updates velocity via {@link applyForce} calls.
+     */
+    public abstract doForces(): void;
+
+    public render(){
+        this.svg.setAttribute("x1", `${this.node1.pos.x}`);
+        this.svg.setAttribute("y1", `${this.node1.pos.y}`);
+        this.svg.setAttribute("x2", `${this.node2.pos.x}`);
+        this.svg.setAttribute("y2", `${this.node2.pos.y}`);
     }
 
 }
@@ -84,23 +103,28 @@ export abstract class GraphEdge<Data extends GraphNodeData> {
  * 
  * This should be enough to get a simulation running.
  */
-export abstract class GraphNode<Data extends GraphNodeData, Edge extends GraphEdge<Data>> {
+export abstract class GraphNode<
+    Data extends GraphNodeData = GraphNodeData, 
+    Edge extends GraphEdge = GraphEdge
+>{
 
     public pos: Vec2 = new Vec2();
     public vel: Vec2 = new Vec2();
 
     public readonly html: HTMLElement;
-    public readonly data: Data
+    public readonly id:  string | number;
+
+    public readonly edges: Edge[] = [];
 
     private _style: CSSStyleDeclaration;
     private readonly manager: GraphManager<Data>;
 
     constructor(
-        manager: GraphManager<Data>,
-        data:    Data
+        manager: GraphManager<Data>, 
+        data: Data
     ){
         this.manager = manager;
-        this.data    = data;
+        this.id      = data.id;
         this.html    = manager.template.cloneNode(true) as HTMLElement;
         this._style  = this.html.style;
     }
@@ -112,11 +136,8 @@ export abstract class GraphNode<Data extends GraphNodeData, Edge extends GraphEd
 
     /**
      * Called after {@link doForces} to update position based on velocity.
-     * Anything goes here.
     */ 
     public abstract doPositioning(): void;
-
-    public abstract get edges(): Edge[];
 
 
     /**
@@ -124,8 +145,8 @@ export abstract class GraphNode<Data extends GraphNodeData, Edge extends GraphEd
      */
     public setPos(x: number, y: number){
         this.pos.setTo(
-            clamp(x,0,this.manager.container.clientWidth),
-            clamp(y,0,this.manager.container.clientHeight)
+            clamp(x,0,this.manager.nodeContainer.clientWidth),
+            clamp(y,0,this.manager.nodeContainer.clientHeight)
         )
     }
 
@@ -142,6 +163,10 @@ export abstract class GraphNode<Data extends GraphNodeData, Edge extends GraphEd
     public render(){
         this._style.left = `${Math.round(this.pos.x)}px`;
         this._style.top  = `${Math.round(this.pos.y)}px`;
+    }
+
+    public clampToContainer(){
+        this.setPos(this.pos.x, this.pos.y);
     }
 
 }
