7ef373b4f0fd68b878ed1d291d6a94e7f5f8ab7f
Fasteroid <29342750+Fasteroid@users.noreply.github.com>
2024-09-16 01:18:54 -0400

    done!

--- src/routes/about/music/+page.svelte
+++ src/routes/about/music/+page.svelte
@@ -7,10 +7,18 @@
     <div class="margins">
         <section>
             <hgroup>
-                <h1>Soundcloud Map</h1>
+                <h1>Soundcloud Map [BETA]</h1>
             </hgroup>
             <div class="p">
-                Description todo
+                This page is an attempt to show the relationships between artists I follow on Soundcloud.<br/>
+                <br/>
+                Every following forms an edge in the graph which pulls its participants closer together—which
+                after some time will pull the whole graph into a semi-stable configuration.<br/>
+                The resulting spatial layout can then be used to infer various social structures such as bands, communities, and collectives.<br/>
+                <br/>
+                This graph can be panned and zoomed by dragging and scrolling, or by touch if you're on mobile.<br/>
+                Clicking or tapping an artist will bring you to their soundcloud page in a new tab.<br/>
+                Hovering an artist will reveal their edges and increase the forces to show how they connect to the bigger picture.<br/>
             </div>
             <SoundcloudGraph/>        
         </section>
