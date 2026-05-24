44cbe3e35f2fd0aae82203c5274939f9f0a621ba
Fasteroid <fasteroid@hotmail.com>
2026-05-23 21:07:24 -0400

    yearly rewording woooo

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -2,6 +2,56 @@
     import { base } from "$app/paths";
 </script>
 
+<style lang="scss">
+
+    .term {
+        display: inline-block;
+        position: relative;
+        font-style: italic;
+        cursor: help;
+
+        .desc {
+            text-decoration: none;
+            position: absolute;
+            width: max-content;
+            font-style: normal;
+            padding: 3px 6px;
+            background-color: rgba(0, 0, 0, 0.418);
+            backdrop-filter: blur(3px);
+            font-size: small;
+            bottom: calc(-100% - 5px);
+            box-sizing: border-box;
+
+            opacity: 0;
+            pointer-events: none;
+
+            transition: opacity 1s ease;
+        }
+
+        .desc::before {
+            content: '💡';
+            display: inline-block;
+            padding-right: 5px;
+        }
+    }
+
+    .term:hover {
+        text-decoration: underline;
+
+        .desc {
+            opacity: 1;
+            pointer-events: all;
+        }
+    }
+
+    .deadlink {
+        color: #da7878; 
+        cursor: pointer; 
+        text-decoration: underline;
+    }
+
+</style>
+
 <body>
     <div class="margins">
         <section>
@@ -13,61 +63,74 @@
                 alt="A 3D-printed replica Expression 2 chip from Garry's Mod.">
                 Hi, I'm Fasteroid!  Nice to meet you!<br>
                 <br>
-                I'm a Computer Science graduate from <a href="https://www.wilmu.edu/">Wilmington University</a>.  
-                I currently work for two companies (one officially, the other cuz they're best friends with the first)
-                and perform a hybrid of programming and engineering work to the beat of my own drum.  I could not
-                have asked for a more perfect profession—in fact sometimes I even get survivor's guilt from it!—why me?
-                So few people get to do what they love for a living—and even those that do are often burdened by bureacracy.  
-                I somehow am one of the lucky few that doesn't have either of these problems.  I wish I could share it somehow.<br>
-                <br>
-                I began writing my first original lines of code early to mid 2014, in a little-known language
-                called Expression&nbsp;2 from the popular sandbox game <i>Garry's Mod</i>.  
-                Expression&nbsp;2 had a very steep learning 
-                curve to it at first due to its obscurity and narrow scope of applications, but by 2015 it had turned
-                into a massive power trip for me.  While I'm not as active on Garry's Mod anymore, E2 is still my go-to 
-                whenever I need to test concepts in a 3D physics environment.  Maybe one of these days I'll learn a proper
-                game engine like Unreal and finally replace it once and for all.<br>
+                I'm a Computer Science graduate from <a href="https://www.wilmu.edu/">Wilmington University</a> (plus some others, it's complicated).  
+                I work for a small software company that collaborates and ships most of its software for a local manufacturing company.  Might sound 
+                boring to some, but honestly it's pretty peak to me.  A small company means no corporate bureacracy, manufacturing means I'm around
+                people who do engineering (which I also love), and I don't have a third thing to say because I didn't use AI to write this crap!<br>
+                <div style="font-size: smaller;">
+                    <br>
+                    (Shame on you for thinking that seeing rule of three or em-dashes in a piece of text automatically means AI wrote it—the reason it writes
+                    like that is because WE write like that!)
+                    <br>
+                    <br>
+                </div>
+                I began writing my first original lines of code early to mid 2014, in a niche language unique to 
+                <div class='term'>
+                    <div class='desc'>A physics sandbox game built on Half-Life 2's game engine, <i>Source</i></div>
+                    Garry's Mod
+                </div> 
+                called Expression&nbsp;2.  It had a very steep learning curve to it at first due to its obscurity and 
+                narrow scope of applications, but by 2015 (once I had begun to understand basic syntax), it had ignited a god complex within me.  
+                I don't claim to know everything, but I'm <i>most certainly</i> better than average.  Experience is shown in practice, not told by
+                a fancy piece of paper or a previous boss.
                 <div style="clear:both"></div>
             </div>
             <div class="p">
                 <img src="{base}/assets/aboutme/ody.jpg" style="float:right; max-height: 245px; margin-right: 0px;"
-                alt="2018 claiming of the Renatra Fusca Creativity Award at DE State Finals.">
-                My most memorable adventures were had during Odyssey of the Mind, which is a 
-                collaborative problem-solving extracurricular for creative thinkers.  Each year, teams of 5-7 choose
-                one of five long-term problems (which all involve some combination of acting and engineering),
-                and are assigned an 8-minute 'spontaneous' problem the day of the performance.<br>
+                alt="2018 claiming of the prestigious Ranatra Fusca Creativity Award at DE State Finals.">
+                My most memorable adventures happened in my highschool era, thanks to <a href="https://odysseyofthemind.com">Odyssey of the Mind</a>,
+                which is a collaborative problem-solving extracurricular for creative thinkers.  
+                Each year, teams of 5-7 choose one of five long-term problems (which all involve some combination of goofy skits and engineering),
+                and upon performance day must tackle the secret 'spontaneous' problem with nothing but their wits and what's provided.<br>
                 <br>
-                With the help of my father as the team coach (who's just there to organize and keep us on track), 
-                and my wonderful teammates, 
-                we managed to be World Finalists four times in a row from 2016 to 2019, with a second place
-                victory for Problem 2 Division III in 2019 (<a href="https://www.odysseyofthemind.com/wf2019/world-finals-2019-scores">scores here</a>).<br>
+                With my dad as team coach (who's only there to keep us on-task and safe–
+                <div class='term'>
+                    <div class='desc'>The team must develop all ideas and props on their own.</div>
+                    outside assistance
+                </div> 
+                is forbidden), and some crazy ideas from my teammates,
+                we reached the prestigious World Finals competition four times in a row, 2016 to 2019, with a second place
+                victory in Problem 2 Division III the final year.  I'd link you the 
+                <a class='deadlink', href="https://www.odysseyofthemind.com/wf2019/world-finals-2019-scores">scores here</a>, 
+                but they've unfortunately been lost to time :^)<br>
                 <br>
                 Odyssey of the Mind taught me a lot—mainly that it usually isn't feasable for one person to work on everything.  
                 Different people have different strengths and weaknesses, and if one person does everything, they're bound to hit 
                 a weakness eventually.  In contrast, with good teamwork and good skill coverage, due to the
-                <a href="https://en.wikipedia.org/wiki/Swiss_cheese_model">swiss cheese model</a>, it is almost impossible to
-                pick a challenge that truly defeats everyone on the team.<br>
-                <br>
-                I really miss those days.  The team has been scattered across the country, everyone is busy with their own lives now, and it
-                just generally feels impossible to keep in touch.<br>
+                <div class='term'>
+                    <div class='desc'>An individual slice may have holes, but with enough stacked it can appear as if there aren't any.</div>
+                    swiss cheese model
+                </div>, it is almost impossible to
+                pick a challenge that truly defeats everyone on the team.  I still try to keep in touch with them, but as adults that have 
+                scattered across the country, it's nearly impossible.<br>
                 <div style="clear:both"></div>
             </div>
             <div class="p">
                 <img src="{base}/assets/aboutme/gmodfriends.jpg" style="float:left; max-height: 245px; margin-left: 0px;" 
                 alt="Friends on E2 Beyond Infinity.">
-                My other fondest memories came from my two Garry's Mod servers, <i>E2 Beyond Infinity</i> and <i>Props Beyond Infinity</i>.  
+                My other fondest memories are from my two Garry's Mod servers, <i>E2 Beyond Infinity</i> and <i>Props Beyond Infinity</i>.  
                 One is a programming and scripting paradise with the occasional player or two, and the other is a prop hunt server that
                 has yet to see itself at max players.<br>
                 <br>
                 One of the coolest features of both these servers is the presence of a bidirectional Discord Relay I designed, which allows seamless two-way 
                 interaction between players on the Garry's Mod server and users in its Discord server.  This relay has proved to serve
                 many practical applications, ranging from posting memes in the in-game chat to running commands remotely from up to 8,000 miles away.  
-                It's not a unique idea by any means, but it's 100% my unique implementation.  It's old, but I'm still proud of it.<br>
+                It's not a unique idea by any means (plenty have done it), but I'm still proud of it because I built it mostly from scratch!<br>
                 <br>
                 In 2021 or 2022 (I forget exactly when), I had acquired enough money via server donations to purchase a new dedicated machine for the server.  
-                This machine runs Linux instead of Windows, and server performance has been significantly better ever since this change.  Most E2 scripts 
-                (which run serverside) run with half the CPU time they used to use.
-                The server also tolerates the other crap we put it through a lot better now, such as spawning hundreds of physics entities in confined spaces.  
+                This machine runs Linux instead of Windows, and was designed with ridiculous single-core performance in mind.  As a result, Expression 2 scripts
+                run with about half the cpu time they used to on the old i7 in the previous machine. The server also tolerates the other crap we put it through
+                a lot better now, such as spawning hundreds of physics entities in confined spaces.  
                 It's mostly dead now, but it still occasionally sees new players, and I still do hang out on it with friends from time to time.  It also barely
                 costs anything to run, so I don't plan on closing it any time soon.<br>
                 <br>
